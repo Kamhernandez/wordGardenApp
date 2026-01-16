3dc0f05a2754490709b04eac0969aef7b8430916
Kamila Hernandez <kaminicoledez@gmail.com>
2026-01-16 10:37:47 -0800

    Initial Commit

---
+++ wordGardenApp/wordGardenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordGardenAppApp.swift
+//  wordGardenApp
+//
+//  Created by HERNANDEZ-CALIX, KAMILA on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct wordGardenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
